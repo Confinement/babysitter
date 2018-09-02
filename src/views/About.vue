57b0d0c2a1e44aa51f1b58d20883e3240f0651f3
Esone <esone.qiu@ringcentral.com>
2018-09-03 00:03:37 +0800

    add webpack-remove-blocks

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <!-- build:start -->
+    <h2>This is an about page prod</h2>
+    <!-- build:end -->
+    <!-- debug:start -->
+    <h2>This is an about page debug</h2>
+    <!-- debug:end -->
   </div>
 </template>
+
+<script>
+  console.log(3344)
+  /* debug:start */
+  console.log(333)
+  /* debug:end */
+  /* build:start */
+  console.log(444)
+  /* build:end */
+  // debug:start
+  console.log(555)
+  // debug:end
+  // build:start
+  console.log(666)
+  // build:end
+</script>
+<style>
+  /* debug:start */
+  h1{color: blue}
+  /* debug:end */
+  /* build:start */
+  h1 {color: red}
+  /* build:end */
+</style>
